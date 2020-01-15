2f8aa4bb8c0efb589c98aa202da09d6abb731e8e
Your Name <your_email@whatever.com>
2020-01-16 05:06:12 +0530

    finishing up

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function getMovies(searchMovie,text){
                 <div class="col-md-3">
                     <div class="well text-center">
                         <img src="${item.Poster}">
-                        <h3>${item.Title}</h3>
+                        <h5>${item.Title}</h5>
                         <a onclick="movieSelected('$(item.imdbID)')" class="btn btn-primary" href="#">Details</a>
                     </div>
                 </div>
@@ -87,6 +87,13 @@ function selector(text){
 
 }
 
+function movieSelected(id){
+    sessionStorage.setItem('movieId',id);
+    window.location='movie.html';
+    return false;
+}
+
+
 var year = 1950;
 var till = 2020;
 var options = "";
@@ -94,3 +101,43 @@ for(var y=year; y<=till; y++){
 options += "<option>"+ y +"</option>";
 }
 document.getElementById("year").innerHTML = options;
+
+function getMovie(){
+    let itemId=sessionStorage.getItem('movieId');
+    axios.get('http://www.omdbapi.com/?i='+itemId+'&apikey=c629e2a8&')
+    .then((response) => {
+        console.log(response);
+        let item=response.data;
+        let output=`
+        <div class="row">
+            <div class="col-md-4">
+                <img src="${item.Poster}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+            <h2>${item.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${item.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${item.Released}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${item.imdbRating}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${item.Actors}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${item.Director}</li>
+            </ul>
+            </div>
+
+        </div>
+        <div class="row">
+        <div class="well">
+          <h3>Plot</h3>
+          ${item.Plot}
+          <hr>
+          <a href="http://imdb.com/title/${item.imdbID}" target="_blank" class="btn btn-success">View IMDB</a>
+          <a href="index.html" class="btn btn-default">Go Back To Search</a>
+        </div>
+      </div>
+        `;
+        $('#movie').html(output);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
